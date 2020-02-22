96ef8516eca491f0b103fea5d0a8dab5bd4f0f58
cameron <cameronlambert98@gmail.com>
2020-02-22 06:15:46 -0800

    fixed a couple bugs and added 'cd path' and 'cd -'

--- bot.py
+++ bot.py
@@ -9,9 +9,11 @@ from dotenv import load_dotenv
 import random
 from random import seed
 
+last_dir = os.getcwd()
+
 client = discord.Client()
 message_size = 2000
-out_size = 200
+out_size = 2000
 proc_timeout = 5
 
 @client.event
@@ -22,12 +24,15 @@ async def on_ready():
         f'{in_guild.name}(id: {in_guild.id})'
     )
 
+
+
 @client.event
 async def on_message(message):
     global proc_timeout
     global client
     global max_size
     global out_size
+    global last_dir
 
     if message.author == client.user:
         return
@@ -40,7 +45,16 @@ async def on_message(message):
     elif message.content[:3] == '!m ' and int(message.content[3:]):
         out_size = int(message.content[3:])
         notification = "output buff size is now: " + str(out_size)
-
+    elif message.content[:5] == '$ cd ':
+        cwd = os.getcwd()
+        if message.content[5:]:
+            if message.content[:6] == '$ cd -':
+                if last_dir:
+                    os.chdir(last_dir)
+            else:
+                last_dir = os.getcwd()
+                os.chdir(message.content[5:])
+        notification = "cd from: " + cwd + " to: " + os.getcwd()
     elif message.content[:2] == '$ ':
         process = subprocess.Popen(message.content[2:],
                     stdout=subprocess.PIPE,
@@ -60,9 +74,14 @@ async def on_message(message):
         output_arr = [response[i:i+message_size] for i in range(0, len(response), message_size)]
         to_print = out_size
         nb_of_messages = int(to_print / message_size) + 1
+        total_printed = 0
         for i in range(len(output_arr[:nb_of_messages])):
-            await message.channel.send(output_arr[i][:to_print])
+            chunk_len = len(output_arr[i][:to_print])
+            if chunk_len > 0:
+                total_printed += chunk_len
+                await message.channel.send(output_arr[i][:to_print])
             to_print -= message_size
+        await message.channel.send("total printed: " + str(total_printed) + "/" + str(len(response)))
     else:
         notification = "usage: \'$ shell exp\' or \'!t int\' for timeout or \'!m int\' for max print amt"
     if notification:
